ca2678b23dee4b4095404c659027498b6caa06b0
Mahmoud Maher <mahmoudmaherfcih30051994@gmail.com>
2024-06-14 02:41:45 +0300

    sales installments dashboard

--- resources/views/dashboard/sales/show.blade.php
+++ resources/views/dashboard/sales/show.blade.php
@@ -2,7 +2,28 @@
 
 @section('title') {{__('sales.plural')}} @endsection
 
-@section('css') @endsection
+@section('css')
+    <!-- Internal Data table css -->
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+    <style>
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+            -moz-appearance: textfield;
+        }
+    </style>
+@endsection
 
 @section('page-header')
     <!-- breadcrumb -->
@@ -105,6 +126,81 @@
         <!--/div-->
     </div>
     <!-- /row -->
+
+    @if($sale->type == App\Enums\SaleTypeEnum::INSTALLMENT)
+    <!-- row opened -->
+    <div class="row row-sm">
+        <!--div-->
+        <div class="col-xl-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table text-md-nowrap" id="example2">
+                            <thead>
+                            <tr>
+                                <th class="wd-15p border-bottom-0">{{ __('sale_installments.attributes.id') }}</th>
+                                <th class="wd-15p border-bottom-0">{{ __('sale_installments.attributes.title') }}</th>
+                                <th class="wd-15p border-bottom-0">{{ __('sale_installments.attributes.value') }}</th>
+                                <th class="wd-15p border-bottom-0">{{ __('sale_installments.attributes.due_date') }}</th>
+                                <th class="wd-15p border-bottom-0">{{ __('sale_installments.attributes.status') }}</th>
+                                <th class="wd-15p border-bottom-0">{{ __('sale_installments.actions.actions') }}</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($sale->installments as $sale_installment)
+                                <tr>
+                                    <td>{{$sale_installment->id}}</td>
+                                    <td>{{$sale_installment->getTranslation('title', app()->getLocale())}}</td>
+                                    <td>{{$sale_installment->value}}</td>
+                                    <td>{{$sale_installment->due_date}}</td>
+                                    <td>
+                                        <span class="badge {{$sale_installment->status->color()}}">{{$sale_installment->status->trans()}}</span>
+                                    </td>
+                                    <td>
+{{--                                        @include('dashboard.sale_installments.partials.actions.edit')--}}
+{{--                                        @include('dashboard.sale_installments.partials.actions.delete')--}}
+                                    </td>
+                                </tr>
+
+{{--                                @include('dashboard.sale_installments.partials.models.edit')--}}
+{{--                                @include('dashboard.sale_installments.partials.models.delete')--}}
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div><!-- bd -->
+            </div><!-- bd -->
+        </div>
+        <!--/div-->
+    </div>
+    <!-- /row -->
+    @endif
 @endsection
 
-@section('js') @endsection
+@section('js')
+    <!-- Internal Data tables -->
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jszip.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+    <!--Internal  Datatable js -->
+    <script src="{{URL::asset('dashboard/assets/js/table-data.js')}}"></script>
+    <!--Internal  Datepicker js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/jquery-ui/ui/widgets/datepicker.js')}}"></script>
+    <!-- Internal Select2 js-->
+    <script src="{{URL::asset('dashboard/assets/plugins/select2/js/select2.min.js')}}"></script>
+    <!-- Internal Modal js-->
+    <script src="{{URL::asset('dashboard/assets/js/modal.js')}}"></script>
+@endsection
