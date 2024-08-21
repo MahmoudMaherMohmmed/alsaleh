ad1ee90aa5b2599e4d9080b6d82b3ed606d25631
Mahmoud Maher <mahmoudmaherfcih30051994@gmail.com>
2024-08-21 18:08:20 +0300

    update notifications

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -24,14 +24,15 @@ function fcm_notification($firebase_id, $title, $body)
         ]
     ];
 
+    $headers = [
+        'Authorization: Bearer ' . get_fcm_access_token(),
+        'Content-Type: application/json',
+    ];
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/v1/projects/al-saleh/messages:send');
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Authorization' => 'Bearer ' . get_fcm_access_token(),
-        'Content-Type' => 'application/json',
-    ]);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['message' => $fcm_message]));
